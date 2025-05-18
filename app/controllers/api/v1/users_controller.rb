8857c66bd0740172916d09f840ff57cd4bfa9ad8
Ursu Tudor <tudor_ursu90@yahoo.com>
2025-05-18 21:37:56 +0300

    Fix UsersController to not expose user ids

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    render json: User.all
+    users = User.all
+    render json: users.map { |user| user_data(user) }
+  end
+
+  private
+
+  def user_data(user)
+    {
+      name: user.name,
+      uuid: user.uuid
+      # avatar: user.avatar
+    }
   end
 end
