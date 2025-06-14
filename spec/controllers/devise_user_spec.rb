fcadfd2c0ccc7ca1d9f8cdd76d8ea098c30edfd3
Ursu Tudor <tudor_ursu90@yahoo.com>
2025-06-14 15:26:16 +0300

    Add pagy tests for usersController

--- spec/controllers/devise_user_spec.rb
+++ spec/controllers/devise_user_spec.rb
@@ -30,23 +30,30 @@ describe "UsersController", type: :request do
     expect(user.uuid).to be_present
   end
 
+  it "return an array of users with pagination, skipping current_user" do
+    logged_user = create(:user)
+    sign_in(logged_user)
+    create_list(:user, 39)
+
+    get "/api/v1/users/list?page=1"
+    expect(JSON.parse(response.body)["users"].length).to be(19)
+
+    get "/api/v1/users/list?page=2"
+    expect(JSON.parse(response.body)["users"].length).to be(20)
+  end
+
   it "returns an array of user + last message data of the users the current_user has a chat with" do
-    user1 = create(:user)
-    user2 = create(:user, email: "dave2@mail.com")
-    user3 = create(:user, email: "dave3@mail.com")
-    sign_in(user1)
+    logged_user = create(:user)
+    sign_in(logged_user)
+    receiver = create(:user)
 
-    post "/api/v1/chats/open", params: { chat: { receiver_uuid: user2.uuid } }
-    post "/api/v1/chats/open", params: { chat: { receiver_uuid: user3.uuid } }
+    create_list(:chat, 40)
 
-    create(:message, chat: Chat.first, content: "first message", user: user1)
-    create(:message, chat: Chat.first, content: "last message of first chat", user: user1)
-    create(:message, chat: Chat.second, content: "second message", user: user2)
-
-    get "/api/v1/users/chats"
-    chat_users = JSON.parse(response.body)["chat_users"]
-
-    expect(chat_users.map { |user| user["last_message"] }).to include("last message of first chat", "second message")
+    get "/api/v1/users/chats?page=2"
+    first_data_set = JSON.parse(response.body)["chat_users"]
+    expect(first_data_set.length).to be(20)
+    expect(first_data_set[0]["last_message"]).to eq("Hello")
+    expect(first_data_set[0]["uuid"]).to eq(receiver.uuid)
   end
 
 
@@ -65,5 +72,24 @@ describe "UsersController", type: :request do
     chat_users = JSON.parse(response.body)["chat_users"]
 
     expect(chat_users.length).to be(1)
+  end
+
+  it "returns logged user info" do
+    user = create(:user, description: "fake user")
+    user.avatar.attach(
+      io: File.open(Rails.root.join('public', 'icon.png')),
+      filename: "icon.png",
+      content_type: "image/png"
+    )
+
+    sign_in(user)
+
+    get "/api/v1/users/current"
+    user_data = JSON.parse(response.body)
+
+    expect(user_data["name"]).to eq(user.name)
+    expect(user_data["description"]).to eq(user.description)
+    expect(user_data["uuid"]).to eq(user.uuid)
+    expect(user_data["avatar"]).to eq(url_for(user.avatar))
   end
 end
